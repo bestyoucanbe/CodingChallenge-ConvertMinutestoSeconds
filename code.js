ac98638f1c06484b087f71bd473bb52d499938c6
Joy Ittycheriah <bestyoucanbe@yahoo.com>
2020-04-13 19:23:00 -0500

    edabit challenge--converts minutes to seconds

---
+++ code.js
@@ -0,0 +1,17 @@
+function convert(minutes) {
+    // Write a function that takes an integer minutes and converts it to seconds.
+    let seconds = minutes * 60
+    return seconds
+}
+
+// Test.assertEquals(convert(6), 360)
+// Test.assertEquals(convert(4), 240)
+// Test.assertEquals(convert(8), 480)
+// Test.assertEquals(convert(60), 3600)
+
+// Tests passed:
+
+// Test Passed: Value == 360
+// Test Passed: Value == 240
+// Test Passed: Value == 480
+// Test Passed: Value == 3600
